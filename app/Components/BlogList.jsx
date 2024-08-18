ee510fd9cf563ac2ce26068b7e7cc1efb5428967
robiulsagor <robiul100.me@gmail.com>
2024-08-18 23:26:06 +0600

    category based filtering done

--- app/Components/BlogList.jsx
+++ app/Components/BlogList.jsx
@@ -1,29 +1,73 @@
+"use client";
+
+import { useState } from "react";
 import { blogData } from "../Assets/assets";
 import BlogItem from "./BlogItem";
 
 const BlogList = () => {
+  const [menu, setMenu] = useState("All");
+
   return (
     <div>
       <div className="flex justify-center gap-4 sm:gap-6 overflow-scroll sm:overflow-hidden my-10 text-xs sm:text-sm md:text-base">
-        <button className="px-4 py-1 rounded-sm bg-black text-white">
+        <button
+          onClick={() => setMenu("All")}
+          className={
+            menu === "All" ? "px-4 py-1 rounded-sm bg-black text-white" : ""
+          }
+        >
           All
         </button>
-        <button>CSS</button>
-        <button>JavaScript</button>
-        <button>ReactJS</button>
-        <button>Development</button>
+        <button
+          onClick={() => setMenu("CSS")}
+          className={
+            menu === "CSS" ? "px-4 py-1 rounded-sm bg-black text-white" : ""
+          }
+        >
+          CSS
+        </button>
+        <button
+          onClick={() => setMenu("JavaScript")}
+          className={
+            menu === "JavaScript"
+              ? "px-4 py-1 rounded-sm bg-black text-white"
+              : ""
+          }
+        >
+          JavaScript
+        </button>
+        <button
+          onClick={() => setMenu("ReactJS")}
+          className={
+            menu === "ReactJS" ? "px-4 py-1 rounded-sm bg-black text-white" : ""
+          }
+        >
+          ReactJS
+        </button>
+        <button
+          onClick={() => setMenu("Development")}
+          className={
+            menu === "Development"
+              ? "px-4 py-1 rounded-sm bg-black text-white"
+              : ""
+          }
+        >
+          Development
+        </button>
       </div>
 
       <div className="flex flex-wrap justify-around gap-5 gap-y-10 mb-16 xl:mx-24">
-        {blogData.map((item, index) => (
-          <BlogItem
-            key={index}
-            title={item.title}
-            description={item.description}
-            image={item.image}
-            category={item.category}
-          />
-        ))}
+        {blogData
+          .filter((item) => (menu === "All" ? true : item.category === menu))
+          .map((item, index) => (
+            <BlogItem
+              key={index}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+              category={item.category}
+            />
+          ))}
       </div>
     </div>
   );
